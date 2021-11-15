369724d475cabc925f5a4bb3ca591ebde48f2423
Akri <vpotach@gmail.com>
2021-11-15 23:36:30 +0300

    userID must be unique

--- models/model.js
+++ models/model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const newUser = new Schema ({
     userTag: { type: String, required: true },
-    userID: { type: String, required: true },
+    userID: { type: String, required: true, unique: true },
     IQ: { type: Number, default: 95 },
     coef: { type: Number, default: 0 },
     startedTime: { type: Number },
